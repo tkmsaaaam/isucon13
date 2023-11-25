95fc2c2dffd4f14f80d3ce34aad64ac94f7cba61
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-11-25 12:58:04 +0900

    use single quote

--- sql/init.sh
+++ sql/init.sh
@@ -78,7 +78,7 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
-echo "CREATE INDEX records_idx ON records(`disabled`,`name`,`domain_id`);" | mysql -u"$ISUCON_DB_USER" \
+echo 'CREATE INDEX records_idx ON records(`disabled`,`name`,`domain_id`);' | mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
