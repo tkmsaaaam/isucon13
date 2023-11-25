d65d1e768e405f9e9d81a8ec15d396c11995caa2
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-11-25 01:58:12 +0000

    update result.sh

--- result.sh
+++ result.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # output alp
-sudo cat /var/log/nginx/access.log | alp ltsv -m ""
+sudo cat /var/log/nginx/access.log | alp ltsv -m "/api/livestream/[0-9]+/livecomment/[0-9]+/report,/api/livestream/[0-9]+/report,/api/livestream/[0-9]+/livecomment,/api/livestream/[0-9]+/reaction,/api/livestream/[0-9]+/exit,/api/livestream/[0-9]+/ngwords,/api/livestream/[0-9]+/enter,/api/livestream/[0-9]+/moderate,/api/user/[a-zA-Z0-9]+/statistics,/api/user/[a-zA-Z0-9]+/icon,/api/user/[a-zA-Z0-9]+/theme,/api/livestream/[0-9]+"
 # -m "/api/user/[0-9]+/delete,/api/user/[0-9]+"
 
 # output mysql log
